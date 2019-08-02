8f63b9ad3c28441a4b98a739e2bc7809dec2c2f6
Yi Liu <yliu1010@ymd.edu>
2019-08-02 15:10:56 -0400

    08022019

--- src/main/java/demo/domain/Location.java
+++ src/main/java/demo/domain/Location.java
@@ -1,6 +1,23 @@
 package demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.util.Date;
+
+// Json <--> object model <--> (ORM) relational model
+// one to many, many to one, many to many, one to one, entity to entity
+// map columns to one object
+
+// Json to object, object to Json (JSON library Jackson library)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)      //only non-null field in Json from frontend will be mapped to object
+@Entity
+@Data
+@Table(name = "LOCATIONS")
 
 public class Location {
 
@@ -8,14 +25,23 @@ public class Location {
         EXCELLENT, OK, UNRELIABLE, BAD, NOFIX, UNKNOW;
     }
 
-    enum RunnerMovementType{
+    public enum RunnerMovementType{
         STOPPED, IN_MOTION;
     }
 
+    @Id
+    @GeneratedValue
     private long id;
 
+    @Embedded
+    @AttributeOverride(name = "bandMake", column = @Column(name = "Unit_band_make"))
     private UnitInfo unitInfo;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "fmi", column = @Column(name = "medical_fmi")),
+            @AttributeOverride(name = "bfr", column = @Column(name = "medical_bfr"))
+    })
     private MedicalInfo medicalInfo;
 
     private double latitude;
@@ -32,4 +58,13 @@ public class Location {
     private String gearProvide;
     private RunnerMovementType runnerMovementType;
     private String serviceTyper;
+
+    public Location() { this.unitInfo = null; }
+
+    @JsonCreator
+    public Location(@JsonProperty("runningId") String runningId) { this.unitInfo = new UnitInfo(runningId); }
+
+    public Location(UnitInfo unitInfo) { this.unitInfo = unitInfo; }
+
+    public String getRunningId() { return this.unitInfo == null ? null : this.unitInfo.getRunningId(); }
 }
